fc739d34dbeb1ca1219e5ef6532b1efed9310679
Eliezer Christanto - Jessica Nathania <jessica.nathania1995@gmail.com>
2015-09-17 14:05:51 +0700

    Tugas 2 Terbaru

--- FileClient.py
+++ FileClient.py
@@ -7,15 +7,36 @@ s = socket.socket()         # Create a socket object
 host, port = "localhost", 8888
 
 s.connect((host, port))
-s.send("Hello server!")
-f = open('tosend.png','rb')
-print 'Sending...'
-l = f.read(1024)
-while (l):
-    print 'Sending...'
-    s.send(l)
-    l = f.read(1024)
-f.close()
-print "Done Sending"
-print s.recv(1024)
+
+print("Pilih file yang ingin dikirim! \n")
+print("1. List Nama.txt \n")
+print("2. List NIM.txt \n")
+a = raw_input("Pilihan : ")
+
+if a in ["1"]:
+	s.send("Hello server! \r\n ")
+	f = open('Nama.txt','rb')
+	print 'Sending...'
+	l = f.read(1024)
+	while (l):
+		print 'Sending...'
+		s.send(l)
+		l = f.read(1024)
+	f.close()
+	print "Done Sending"
+	print s.recv(1024)
+	
+else:
+	s.send("Hello server! \r\n ")
+	f = open('NIM.txt','rb')
+	print 'Sending...'
+	l = f.read(1024)
+	while (l):
+		print 'Sending...'
+		s.send(l)
+		l = f.read(1024)
+	f.close()
+	print "Done Sending"
+	print s.recv(1024)
+	
 s.close                     # Close the socket when done
